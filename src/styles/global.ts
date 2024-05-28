1d14d814bdef862cf20a48a1ff89d4be5f94683d
arthur3r <arthurfreireribeiro19@gmail.com>
2024-05-27 21:58:31 -0300

    chore: initial setup

---
+++ src/styles/global.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 3px ${({ theme }) => theme.blue};
+    border-radius: 4px;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme['base-background']};
+    color: ${({ theme }) => theme['base-text']};
+    --webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Nunito', sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
