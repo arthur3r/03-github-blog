fbef846061488def9dcfa76c714b2efd949d67ca
arthur3r <arthurfreireribeiro19@gmail.com>
2024-06-04 00:42:25 -0300

    feat: fetch infos from github

--- src/pages/Home/components/Profile/index.tsx
+++ src/pages/Home/components/Profile/index.tsx
@@ -1,40 +1,42 @@
-/* eslint-disable camelcase */
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useCallback, useEffect, useState } from 'react'
+import { api } from '../../../../lib/axios'
+
 import {
   faArrowUpRightFromSquare,
   faUserGroup,
 } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { LinkNavigator } from '../../../../components/LinkNavigator'
-import { useCallback, useEffect, useState } from 'react'
-import { api } from '../../../../lib/axios'
 import {
   ProfileContainer,
   ProfileContent,
   ProfileFooter,
   ProfileHeader,
 } from './styles'
+import { Spinner } from '../../../../components/Spinner'
 
 interface User {
   name: string
-  avatar_url: string
   bio: string
+  avatar_url: string
+  html_url: string
   login: string
   followers: number
-  html_url: string
 }
 
 export function Profile() {
-  const [user, setUser] = useState({} as User)
+  const [user, setUser] = useState<User>({} as User)
+  const [isLoading, setIsLoading] = useState(false)
 
   const fetchUser = useCallback(async () => {
     try {
+      setIsLoading(true)
       const response = await api.get('users/arthur3r')
-      const { name, avatar_url, bio, login, followers, html_url } =
-        response.data
-      setUser({ name, avatar_url, bio, login, followers, html_url })
+      setUser(response.data)
     } catch (error) {
-      console.error(error)
+    } finally {
+      setIsLoading(false)
     }
   }, [])
 
@@ -44,31 +46,35 @@ export function Profile() {
 
   return (
     <ProfileContainer>
-      <ProfileContent>
-        <img className="avatar" src={user.avatar_url} alt="" />
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <ProfileContent>
+          <img className="avatar" src={user.avatar_url} alt="" />
 
-        <section>
-          <ProfileHeader>
-            <h2>{user.name}</h2>
-            <LinkNavigator type="link" to={user.html_url}>
-              github
-              <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-            </LinkNavigator>
-          </ProfileHeader>
-          <p>{user.bio}</p>
-          <ProfileFooter>
-            <div className="item">
-              <FontAwesomeIcon icon={faGithub} />
-              <span>{user.login}</span>
-            </div>
+          <section>
+            <ProfileHeader>
+              <h2>{user.name}</h2>
+              <LinkNavigator type="link" to={user.html_url}>
+                github
+                <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+              </LinkNavigator>
+            </ProfileHeader>
+            <p>{user.bio}</p>
+            <ProfileFooter>
+              <div className="item">
+                <FontAwesomeIcon icon={faGithub} />
+                <span>{user.login}</span>
+              </div>
 
-            <div className="item">
-              <FontAwesomeIcon icon={faUserGroup} />
-              <span>{user.followers} seguidores</span>
-            </div>
-          </ProfileFooter>
-        </section>
-      </ProfileContent>
+              <div className="item">
+                <FontAwesomeIcon icon={faUserGroup} />
+                <span>{user.followers} seguidores</span>
+              </div>
+            </ProfileFooter>
+          </section>
+        </ProfileContent>
+      )}
     </ProfileContainer>
   )
 }
